bec84e38078812fada1fb6003cbc5e00e43b3061
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-02 03:01:20 +0000

    feat: add getApiInfo function

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,47 @@ let characterRepository = (function () {
     }
   }
   //add characters to webpage
+  function addCharacterOnPage (character) {
+    let characters = document.querySelector('all-characters');
 
+    document.createElement('div');
+    document.createElement('button');
+
+    button.innerText = character.name;
+
+    characters.appendChild('div');
+    characters.appendChild('button');
+  }
+  
   //get api info
+  function getApiInfo() {
+    return fetch(api).then(function(response) {
+      return response.json();
+    }).then(function(characters) {
+      json.results.array.forEach(character => { //need to go one level deeper
+        let character = {
+          name: character.name, //need to go one level deeper
+          personality: character.personality,
+          species: character.species,
+          catchPhrase: character.catch-phrase,
+          image: character.image_uri,
+          saying: character.saying
+        };
+        addCharacter(character);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
 
   //get character info
 
   return {
-
+    getAll,
+    addCharacter,
+    addCharacterOnPage,
+    getApiInfo
   };
 })();
+
+characterRepository.getApiInfo();
