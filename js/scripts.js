e1321e5102d22a826e8f339b5185d1d9448ef1ad
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-02 02:10:00 +0000

    chore: write out list of needed functions in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-let characterRepository = function () {
+let characterRepository = (function () {
   let characterList = [];
   let api = "https://studio-ghibli-films-api.herokuapp.com/";
-};
+
+  //get all characters function
+
+  //add new character function
+
+  //add characters to webpage
+
+  //get api info
+
+  //get character info
+
+  return {
+
+  };
+})();
